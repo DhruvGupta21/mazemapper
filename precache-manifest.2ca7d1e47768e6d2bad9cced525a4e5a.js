e5acc486b4d163b54bba5f0cc519e9b5b612281b
DhruvGupta21 <dhruvgenie21@gmail.com>
2024-07-02 01:12:41 +0530

    Updates

---
+++ precache-manifest.2ca7d1e47768e6d2bad9cced525a4e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc62d3e92ae2195a358fa521a09a3ba",
+    "url": "/mazemapper/index.html"
+  },
+  {
+    "revision": "2961d86cb0ec3fc02600",
+    "url": "/mazemapper/static/css/main.291ecd78.chunk.css"
+  },
+  {
+    "revision": "d4de9369087c39067851",
+    "url": "/mazemapper/static/js/2.d3b5e5af.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/mazemapper/static/js/2.d3b5e5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2961d86cb0ec3fc02600",
+    "url": "/mazemapper/static/js/main.e0e9c5ef.chunk.js"
+  },
+  {
+    "revision": "0e62fb334cb3e4c42347",
+    "url": "/mazemapper/static/js/runtime-main.fe484e70.js"
+  }
+]);
